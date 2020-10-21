b40f96b3e19b450c9c8a9999bd3863783e961a6e
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2020-10-21 13:48:01 +0200

    testing2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hellos, Worlds!");
 		System.out.println("Goodbye, World!");
 		//testar
 		
